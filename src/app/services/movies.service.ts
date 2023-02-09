8fd8fb63c696db425227c18baa331139d0e75b31
Natalia Góreczna <natalia.goreczna@capgemini.com>
2023-02-09 15:33:30 +0100

    added dynamic categories

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,13 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { TvDto } from '../models/tv-show.model';
+import { of, switchMap } from 'rxjs';
+import { MovieDto } from '../models/movie.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
+  baseUrl: string = 'https://api.themoviedb.org/3';
+  apiKey = '6345f816de1a2bd8b54098fe5c4f241f';
+
   constructor(private http: HttpClient) {}
 
-  getMovies() {
-    return this.http.get('https://api.themoviedb.org/3/movie/popular?api_key=6345f816de1a2bd8b54098fe5c4f241f');
+  getMovies(type: string = 'popular') {
+    return this.http.get<MovieDto>(`${this.baseUrl}/movie/${type}?api_key=${this.apiKey}`).pipe(
+      switchMap((response) => {
+        return of(response.results);
+      })
+    );
+  }
+
+  getTvs(type: string = 'latest') {
+    return this.http.get<TvDto>(`${this.baseUrl}/tv/${type}?api_key=${this.apiKey}`).pipe(
+      switchMap((response) => {
+        return of(response.results);
+      })
+    );
   }
 }
