f0fd7914a9bed559fc666e106fc341c1698a3a15
Natalia Góreczna <n.w.goreczna@gmail.com>
2024-05-05 23:11:22 +0200

    fix small issues like display rating about the movie

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { first } from 'rxjs';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { first, switchMap } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 import { Movie, MovieCredits, MovieImages, MovieVideo } from 'src/app/models/movie.model';
 import { MoviesService } from 'src/app/services/movies.service';
 import { IMAGES_SIZES } from 'src/app/utils/images.utils';
@@ -17,50 +18,83 @@ export class MovieComponent implements OnInit, OnDestroy {
   movieCredits: MovieCredits | null = null;
   similarMovies: Movie[] = [];
   imagesSizes = IMAGES_SIZES;
+  private subscriptions: Subscription[] = [];
 
   constructor(private route: ActivatedRoute, private moviesService: MoviesService) {}
 
   ngOnInit(): void {
-    this.route.params.pipe(first()).subscribe(({ id }) => {
-      this.getMovie(id);
-      this.getMovieVideos(id);
-      this.getMovieImages(id);
-      this.getMovieCredits(id);
-      this.getMovieSimilar(id);
-    });
+    this.fetchData();
+
+    this.subscriptions.push(
+      this.route.paramMap.pipe(
+        switchMap((params: ParamMap) => {
+          const id = params.get('id') || '';
+          return this.moviesService.getMovie(id);
+        })
+      ).subscribe((movieData: Movie) => {
+        this.movie = movieData;
+      }),
+
+      this.route.paramMap.pipe(
+        switchMap((params: ParamMap) => {
+          const id = params.get('id') || '';
+          return this.moviesService.getMovieVideos(id);
+        })
+      ).subscribe((movieVideosData: MovieVideo[]) => {
+        this.movieVideos = movieVideosData;
+      }),
+
+      this.route.paramMap.pipe(
+        switchMap((params: ParamMap) => {
+          const id = params.get('id') || '';
+          return this.moviesService.getMovieImages(id);
+        })
+      ).subscribe((movieImagesData: MovieImages) => {
+        this.movieImages = movieImagesData;
+      }),
+
+      this.route.paramMap.pipe(
+        switchMap((params: ParamMap) => {
+          const id = params.get('id') || '';
+          return this.moviesService.getMovieCredits(id);
+        })
+      ).subscribe((movieCreditsData: MovieCredits) => {
+        this.movieCredits = movieCreditsData;
+      }),
+
+      this.route.paramMap.pipe(
+        switchMap((params: ParamMap) => {
+          const id = params.get('id') || '';
+          return this.moviesService.getMovieSimilar(id);
+        })
+      ).subscribe((movieSimilarData: Movie[]) => {
+        this.similarMovies = movieSimilarData;
+      })
+    );
   }
 
   ngOnDestroy() {
-    console.log('component destroyed');
+    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 
-  getMovie(id: string) {
-    this.moviesService.getMovie(id).subscribe((movieData) => {
-      this.movie = movieData;
-    });
-  }
-
-  getMovieVideos(id: string) {
-    this.moviesService.getMovieVideos(id).subscribe((movieVideosData) => {
-      this.movieVideos = movieVideosData;
-    });
-  }
-
-  getMovieImages(id: string) {
-    this.moviesService.getMovieImages(id).subscribe((movieImagesData) => {
-      this.movieImages = movieImagesData;
-    });
-  }
-
-  getMovieCredits(id: string) {
-    this.moviesService.getMovieCredits(id).subscribe((movieCreditsData) => {
-      this.movieCredits = movieCreditsData;
-    });
-  }
-
-  getMovieSimilar(id: string) {
-    this.moviesService.getMovieSimilar(id).subscribe((movieSimilarData) => {
-      this.similarMovies = movieSimilarData;
+  private fetchData(): void {
+    this.route.paramMap.pipe(first()).subscribe((params: ParamMap) => {
+      const id = params.get('id') || '';
+      this.moviesService.getMovie(id).subscribe((movieData: Movie) => {
+        this.movie = movieData;
+      });
+      this.moviesService.getMovieVideos(id).subscribe((movieVideosData: MovieVideo[]) => {
+        this.movieVideos = movieVideosData;
+      });
+      this.moviesService.getMovieImages(id).subscribe((movieImagesData: MovieImages) => {
+        this.movieImages = movieImagesData;
+      });
+      this.moviesService.getMovieCredits(id).subscribe((movieCreditsData: MovieCredits) => {
+        this.movieCredits = movieCreditsData;
+      });
+      this.moviesService.getMovieSimilar(id).subscribe((movieSimilarData: Movie[]) => {
+        this.similarMovies = movieSimilarData;
+      });
     });
   }
 }
